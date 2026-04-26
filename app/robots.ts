3966bc55181a9471d456889ab38b6654e01136e4
Nnamdi <nnamdi4nwosu@gmail.com>
2026-04-26 01:58:01 +0100

    added favicon and robot to hide irrelevant pages

--- app/robots.ts
+++ app/robots.ts
@@ -1,10 +1,18 @@
-import { MetadataRoute } from 'next';
-
-export default function robots(): MetadataRoute.Robots {
+export default function robots() {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: [
+        '/privacy',
+        '/terms',
+        '/support',
+        '/cookies',
+        '/eula',
+        '/cookies-policy',
+        '/delete-account',
+        '/onboarding/', // Blocks the entire onboarding folder
+      ],
     },
     sitemap: 'https://churza.org/sitemap.xml',
   };
